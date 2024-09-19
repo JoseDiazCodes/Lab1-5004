8d236f114574390288edb8f520317dbd145c72c1
JoseDiazCodes <josediazjr.a@gmail.com>
2024-09-19 18:05:20 -0400

    made some changes

--- src/stock/Stock.java
+++ src/stock/Stock.java
@@ -98,7 +98,7 @@ public class Stock {
   public String toString() {
     double changePercent = getChangePercent() * 100;
     return String.format(
-            "%s Current Price: $%.2f\n Gain/Loss: %.2f%%",
+            "%s Current Price: $ %.2f\n Gain/Loss: %.2f%%",
             name, currentPrice, changePercent
     );
   }
